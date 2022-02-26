45bd6a8d2b74183701e1d1f5d18c304465891d09
SebastianBuritica <93333842+SebastianBuritica@users.noreply.github.com>
2022-02-26 16:22:23 -0500

    1

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const scoreDisplay = document.querySelectorAll('span')
+    const startBtn = document.querySelector('.start')
+
+    
+})
