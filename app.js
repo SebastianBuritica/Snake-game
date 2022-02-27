13904c883c5a4f367e2984f4124d8e7e1b92919e
SebastianBuritica <93333842+SebastianBuritica@users.noreply.github.com>
2022-02-26 20:17:21 -0500

    2

--- app.js
+++ app.js
@@ -48,13 +48,32 @@ document.addEventListener('DOMContentLoaded', () => {
         squares[tail].classList.remove('snake') //removes class of snake from the TAIL
         currentSnake.unshift(currentSnake[0] + direction) //gives direction to the head of the array
 
-        if(squares[currentSnake[0]].classList.contains('apple'))
+        
+        
+        //deals with snake getting apple
+        if(squares[currentSnake[0]].classList.contains('apple')) {
+            squares[currentSnake[0]].classList.remove('apple')
+            squares[tail].classList.add('snake')
+            currentSnake.push(tail)
+            // randomApple()
+            score++
+            scoreDisplay.textContent = score
+            cleanInterval(interval)
+            intervalTime = intervalTime * speed
+            interval = setInterval(moveOutcomes, intervalTime)
+        }
 
-
-     //deals with snake getting apple
+        squares[currentSnake[0]].classList.add('snake')
     }
     
 
+    //Generate new apple once apple is eaten
+    function randomApple() {
+        do{
+            appleIndex = Math.floor(Math.random() *squares.length)
+        } while(squares[appleIndex].classList.contains('snake')) //making sure apples
+        squares[appleIndex].classList.add('apple')
+    }
 
 
     //Assign functions to keycodes
@@ -66,12 +85,13 @@ document.addEventListener('DOMContentLoaded', () => {
         } else if (e.keyCode === 38) {
             direction = -width // if we press the up arrow, the snake will go back ten divs, appearing to go up 
         } else if (e.keyCode === 37) {
-            direction -1 // if we press left, the snake will go left one div
+            direction = -1 // if we press left, the snake will go left one div
         } else if (e.keyCode === 40) {
             direction = +width //if we press down, the snake head will instantly appear in the div ten divs from where you are now
         } 
     }
 
     document.addEventListener('keyup', control)
+    startBtn.addEventListener('click', startGame)
 
 })
