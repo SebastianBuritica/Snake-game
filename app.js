c249ff2408896788bb0ce9bab96f29d84785ea5a
SebastianBuritica <93333842+SebastianBuritica@users.noreply.github.com>
2022-02-27 15:38:47 -0500

    v1

--- app.js
+++ app.js
@@ -36,9 +36,9 @@ document.addEventListener('DOMContentLoaded', () => {
      //deals with snake hitting border and snake hitting self
         if (
             (currentSnake[0] + width >= (width * width) && direction === width) || //if snake hits bottom
-            (currentSnake[0] % width === width -1 && direction === 1) //if snake hits right wall
-            (currentSnake[0] % width === 0 && direction === -1) //if snake hits left wall
-            (currentSnake[0] - width < 0 && direction === -width) //if snake hits the top
+            (currentSnake[0] % width === width -1 && direction === 1) || //if snake hits right wall
+            (currentSnake[0] % width === 0 && direction === -1) || //if snake hits left wall
+            (currentSnake[0] - width < 0 && direction === -width) || //if snake hits the top
             squares[currentSnake[0] + direction].classList.contains('snake') //if snake goes into itself
         ) {
             return clearInterval(interval) //this will clear the interval if any of the above happen
